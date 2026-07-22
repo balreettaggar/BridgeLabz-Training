d94c8fb6cbb106b7939c77a11a8d4ec4845b589c
balreettaggar <balreetttaggar@gmail.com>
2026-07-22 15:21:51 +0530

    Balreet : Setting up code repo

---
+++ core-csharp-practice/Patterns.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace core_csharp_practice
+{
+    public class Patterns
+    {
+
+        public static void StarPattern()
+        {
+            int n = 5;
+
+            for (int i = 0; i < n; i++)
+            {
+
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ core-csharp-practice/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+using core_csharp_practice;
+
+Patterns.StarPattern();
