42e1c6e8bd10b4dd8132ab4d10d9e4bbd10a9ece
balreettaggar <balreetttaggar@gmail.com>
2026-08-07 21:12:25 +0530

    Added NUnit testing assignments and unit tests

---
+++ TestProject1/CalculatorTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ClassLibrary1;
+namespace TestProject1
+{
+    //internal class CalculatorTests
+    //{
+    //    private Calculator calculator;
+    //    [SetUp]
+    //    public void SetUp()
+    //    {
+    //        calculator = new Calculator();
+    //    }
+
+    //    [Test]
+    //    public void AddTest()
+    //    {
+    //        int result = calculator.Add(2, 5);
+    //        Assert.AreEqual(result, 7);
+    //    }
+
+    //    [Test]
+    //    public void SubTest()
+    //    {
+    //        int result = calculator.Sub(2, 5);
+    //        Assert.AreEqual(result, -3);
+    //    }
+
+    //    [Test]
+    //    public void MulTest()
+    //    {
+    //        int result = calculator.Mul(2, 5);
+    //        Assert.AreEqual(result, 10);
+    //    }
+
+    //    [Test]
+    //    public void DivTest()
+    //    {
+    //        int result = calculator.Div(2, 5);
+    //        Assert.AreEqual(result, 0);
+    //    }
+
+    //    [Test]
+    //    public void Div_ByZero_ShouldThrowException()
+    //    {
+    //        Assert.Throws<DivideByZeroException>(
+    //            () => calculator.Div(10, 0)
+    //        );
+    //    }
+    //}
+}
---
+++ TestProject1/ListManagerTests.cs
@@ -0,0 +1,55 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace TestProject1
+{
+    internal class ListManagerTests
+    {
+        private ListManager listManager;
+
+        [SetUp]
+        public void SetUp()
+        {
+            listManager = new ListManager();
+        }
+
+        [Test]
+        public void AddElementTest()
+        {
+            List<int> myList = [ 1, 2, 3, 4, 5 ];
+            listManager.AddElement(myList, 6);
+            Assert.Contains(6, myList);
+            //Console.WriteLine(myList);
+            for(int i=0; i<myList.Count; i++)
+            {
+                Console.WriteLine(myList[i]+" ");
+            }
+        }
+
+        [Test]
+        public void RemoveElementTest()
+        {
+            List<int> myList = [1, 2, 3, 4, 5];
+            listManager.RemoveElement(myList, 3);
+            Assert.IsFalse(myList.Contains(3));
+            //Console.WriteLine(myList);
+            for (int i = 0; i < myList.Count; i++)
+            {
+                Console.WriteLine(myList[i] + " ");
+            }
+        }
+
+        [Test]
+        public void GetSizeTest()
+        {
+            List<int> myList = [1, 2, 3, 4, 5];
+            int size = listManager.GetSize(myList);
+            Assert.AreEqual(5, size);
+            Console.WriteLine(myList.Count);
+        }
+    }
+}
