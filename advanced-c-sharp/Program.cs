81ca309a396cc02e6218b9c3cb0c992e59a9282b
balreettaggar <balreetttaggar@gmail.com>
2026-08-08 23:01:15 +0530

    introductory generic problems

---
+++ advanced-c-sharp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using advanced_c_sharp;
+using System.Text.RegularExpressions;
+
+//RegexProblems.ValidateUsername();
+RegexProblems.LicensePlate();
