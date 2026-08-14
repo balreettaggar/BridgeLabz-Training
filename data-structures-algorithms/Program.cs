d4afea73a74b03e40c5d6167e9dea22f14e373f9
balreettaggar <balreetttaggar@gmail.com>
2026-08-14 23:01:56 +0530

    solved sorting problems

--- data-structures-algorithms/Program.cs
+++ data-structures-algorithms/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+using data_structures_algorithms;
+
+//Sorting.BubbleSort();
+Sorting.SelectionSort();
---
+++ data-structures-algorithms/Sorting.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_structures_algorithms
+{
+    static internal class Sorting
+    {
+        static internal void BubbleSort()
+        {
+            int[] arr = { 5, 2, 9, 1, 19, 13, 14, 23, 10, 21 };
+            for(int i=0; i<arr.Length; i++)
+            {
+                for(int j=i+1; j<arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+
+            foreach(int num in arr)
+            {
+                Console.Write(num + " ");
+            }
+        }
+
+        internal static void SelectionSort()
+        {
+            int[] arr = { 5, 2, 9, 1, 19, 13, 14, 23, 10, 21, 43, 32 };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int minIndex = i;
+                for (int j = i; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i]+" ");
+            }
+        }
+
+        internal static void InsertionSort()
+        {
+
+        }
+    }
+}
